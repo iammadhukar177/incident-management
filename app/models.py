a4fafaa2c96b95130c3b0eff1c281c9437b8b3d9
MADHUKAR SAMMETA <155879431+iammadhukar177@users.noreply.github.com>
2025-08-26 13:36:07 +0530

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,16 @@
+from . import db
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    role = db.Column(db.String(20), nullable=False)
+
+class Incident(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    status = db.Column(db.String(20), default="Open")
+    assigned_to = db.Column(db.String(80))
+    created_by = db.Column(db.String(80))
